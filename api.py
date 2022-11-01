73e50d1c1003a472d3096d00aad1648cb575fa54
t.bonfiglio <bonfiglio.tomas@gmail.com>
2022-10-31 23:26:17 -0300

    Updates generales

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 # From https://www.datacamp.com/tutorial/machine-learning-models-api-python
 from flask import Flask, request, jsonify
-import flask_monitoringdashboard as dashboard # docu en https://flask-monitoringdashboard.readthedocs.io/en/latest/configuration.html y https://flask-monitoringdashboard.readthedocs.io/en/latest/functionality.html
+import flask_monitoringdashboard as dashboard # docu en https://flask-monitoringdashboard.readthedocs.io/en/latest/configuration.html y https://flask-monitoringdashboard.readthedocs.io/en/latest/functionality.html VER CONFIG EN https://github.com/flask-dashboard/Flask-MonitoringDashboard/blob/master/config.cfg
 import pickle
 import traceback
 import pandas as pd
@@ -10,23 +10,46 @@ import numpy as np
 app = Flask(__name__)
 
 dashboard.bind(app)
-@app.route('/predict', methods=['POST'])
-def predict():
-    if rf:
+@app.route('/predict/v1/<model>', methods=['POST'])
+def predict(model):
+    
+    model_columns = pickle.load(open('saved_models/columns_01.pkl', 'rb'))
+
+    if model=='lr_01':
+        
+        lr = pickle.load(open('saved_models/lr_model_01.pkl', 'rb'))
+
         try:
             json_ = request.json
             query = pd.DataFrame(json_)
             query = query.reindex(columns=model_columns, fill_value=0)
+            
+            prediction = list(lr.predict(query))
 
-            prediction = list(rf.predict(query))
-
-            return jsonify({'prediction': str(prediction)})
+            return jsonify({'prediction': str(prediction), 'model': 'lr_01'})
 
         except:
 
             return jsonify({'trace': traceback.format_exc()})
+
+    elif model=='rf_01':
+        
+        rf = pickle.load(open('saved_models/rf_model_01.pkl', 'rb'))
+            
+        try:    
+            json_ = request.json
+            query = pd.DataFrame(json_)
+            query = query.reindex(columns=model_columns, fill_value=0)
+            
+            prediction = list(rf.predict(query))
+
+            return jsonify({'prediction': str(prediction), 'model': 'rf_01'})
+
+        except:
+
+            return jsonify({'trace': traceback.format_exc()})        
     else:
-        print ('Train the model first')
+
         return ('No model here to use')
 
 if __name__ == '__main__':
@@ -34,10 +57,5 @@ if __name__ == '__main__':
         port = int(sys.argv[1]) # This is for a command-line input
     except:
         port = 12345 # If you don't provide any port the port will be set to 12345
-
-    rf = pickle.load(open('saved_models/model_01.pkl', 'rb')) # Load "model.pkl"
-    print ('Model loaded')
-    model_columns = pickle.load(open('saved_models/columns_01.pkl', 'rb')) # Load "model_columns.pkl"
-    print ('Model columns loaded')
 
     app.run(port=port, debug=True)
