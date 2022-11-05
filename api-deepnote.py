9a8dc630bab8c0902ff975b535cd40cac27da50c
Tomas Bonfiglio <bonfiglio.tomas@gmail.com>
2022-11-05 19:33:19 +0000

    WIP: prediccion precalculada

--- api-deepnote.py
+++ api-deepnote.py
@@ -19,7 +19,7 @@ import numpy as np
 app = Flask(__name__)
 
 dashboard.bind(app)
-@app.route('/predict/v1/<model>', methods=['POST'])
+@app.route('/predict/saved_models/v1/<model>', methods=['POST'])
 def predict(model):
     
     model_columns = pickle.load(open('saved_models/columns_01.pkl', 'rb'))
@@ -32,7 +32,7 @@ def predict(model):
             json_ = request.json
             query = pd.DataFrame(json_)
             query = query.reindex(columns=model_columns, fill_value=0)
-            
+
             prediction = list(lr.predict(query))
 
             return jsonify({'prediction': str(prediction), 'model': 'lr_01'})
@@ -63,7 +63,57 @@ def predict(model):
 
 @app.route('/bienvenida/<nombre>', methods=['POST', 'GET'])
 def hello_not_really_stranger(nombre):
-    return f"Hola {nombre}! viste que tremenda API?"
+    return f"Hola {nombre}! viste que tremenda API? (by the way usaste el método {request.method})"
+
+@app.route('/predict/saved_preds/v01/<model>', methods=['POST'])
+def retrieve(model):
+    
+    
+    model_columns = pickle.load(open('saved_models/columns_01.pkl', 'rb'))
+
+    if model=='lr_01':
+        
+        lr = pickle.load(open('saved_models/lr_model_01.pkl', 'rb'))
+
+        try:
+            json_ = request.json
+            query = pd.DataFrame(json_)
+            query = query.reindex(columns=model_columns, fill_value=0)
+
+            prediction = list(lr.predict(query))
+
+            return jsonify({'prediction': str(prediction), 'model': 'lr_01'})
+
+        except:
+
+            return jsonify({'trace': traceback.format_exc()})
+
+    elif model=='rf_01':
+        
+        rf = pickle.load(open('saved_models/rf_model_01.pkl', 'rb'))
+            
+        try:    
+            json_ = request.json
+            query = pd.DataFrame(json_)
+            query = query.reindex(columns=model_columns, fill_value=0)
+            
+            prediction = list(rf.predict(query))
+
+            return jsonify({'prediction': str(prediction), 'model': 'rf_01'})
+
+        except:
+
+            return jsonify({'trace': traceback.format_exc()})        
+    else:
+
+        return ('No model here to use')
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
 
